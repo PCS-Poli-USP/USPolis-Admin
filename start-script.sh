6979fb818767d9d458768ebaeac67166c14c9e9a
Henrique Duran <henriqueduran15@gmail.com>
2023-09-26 12:40:01 -0300

    :wrench: start-script: now removes the pid file before write

--- start-script.sh
+++ start-script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 python3 ./USPolis-Admin-Backend/wsgi.py &
+rm backend-pid
 echo "$!" >> backend-pid
 
 npm -C ./USPolis-Admin-Frontend/ run start &
+rm frontend-pid
 echo "$!" >> frontend-pid
 
 tail -f /dev/null
